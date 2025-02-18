b5681e7befedc4851d7e1f9374d7770f4981aff9
slimesito <williamonfroy@gmail.com>
2025-02-18 14:27:50 -0400

    modificación de seguridad

--- config/database.php
+++ config/database.php
@@ -74,7 +74,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'AL32UTF8'),
             'prefix' => env('DB_PREFIX', ''),
-            'prefix_schema' => env('DB_SCHEMA_PREFIX', 'SIRA'),
+            'prefix_schema' => env('DB_SCHEMA_PREFIX', ''),
             'edition' => env('DB_EDITION', 'ora$base'),
             'server_version' => env('DB_SERVER_VERSION', '11g'),
             'load_balance' => env('DB_LOAD_BALANCE', 'yes'),
