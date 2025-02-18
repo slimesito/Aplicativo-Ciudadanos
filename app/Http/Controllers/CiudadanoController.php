a9b19991647de3f5e9f78eb87a577b2574e65494
slimesito <williamonfroy@gmail.com>
2025-02-18 14:56:59 -0400

    modificación 3

--- app/Http/Controllers/CiudadanoController.php
+++ app/Http/Controllers/CiudadanoController.php
@@ -15,7 +15,7 @@ class CiudadanoController extends Controller
         $query = $request->input('query');
         $fullId = $nationality . str_pad($query, 9, '0', STR_PAD_LEFT);
 
-        $ciudadanos = Ciudadano::where('id_ciudadano', '=', $fullId)->get();
+        $ciudadanos = Ciudadano::where('id', '=', $fullId)->get();
 
         if ($ciudadanos->isEmpty()) {
             return redirect()->back()->with('message', 'Esta Cédula no se encuentra registrada.');
@@ -42,28 +42,28 @@ class CiudadanoController extends Controller
 
     public function store(Request $request)
     {
-        $id_ciudadano = $request->nacionalidad . $request->id_ciudadano;
+        $id = $request->nacionalidad . $request->id;
 
-        while (strlen($id_ciudadano) < 10) {
-            $id_ciudadano = $request->nacionalidad . '0' . substr($id_ciudadano, 1);
+        while (strlen($id) < 10) {
+            $id = $request->nacionalidad . '0' . substr($id, 1);
         }
 
-        $request->merge(['id_ciudadano' => $id_ciudadano]);
+        $request->merge(['id' => $id]);
 
         $request->validate([
-            'id_ciudadano' => 'required|unique:CIUDADANO',
+            'id' => 'required|unique:CIUDADANO',
             'primer_nombre' => 'required|max:255',
             'segundo_nombre' => 'max:255',
             'primer_apellido' => 'required|max:255',
             'segundo_apellido' => 'max:255',
             'sexo' => 'required|in:M,F',
             'fecha_nacimiento' => 'required|date',
-            'id_estado_civil' => 'required|in:1,2,3,4,5,6',
+            'estado_civil' => 'required|in:1,2,3,4,5,6',
             'fecha_fallecimiento' => 'date|nullable',
         ],
         [
-            'id_ciudadano.required' => 'La Cédula es obligatoria.',
-            'id_ciudadano.unique' => 'Esta Cédula ya se encuentra registrada en la base de datos.',
+            'id.required' => 'La Cédula es obligatoria.',
+            'id.unique' => 'Esta Cédula ya se encuentra registrada en la base de datos.',
             'primer_nombre.required' => 'El Primer Nombre es obligatorio.',
             'primer_nombre.max' => 'El Primer Nombre no puede tener más de 255 caracteres.',
             'segundo_nombre.max' => 'El Segundo Nombre no puede tener más de 255 caracteres.',
@@ -74,8 +74,8 @@ class CiudadanoController extends Controller
             'sexo.in' => 'El Sexo debe ser Masculino o Femenino.',
             'fecha_nacimiento.required' => 'La Fecha de Nacimiento es obligatoria.',
             'fecha_nacimiento.date' => 'La Fecha de Nacimiento debe ser una fecha válida.',
-            'id_estado_civil.required' => 'El Estado Civil es obligatorio.',
-            'id_estado_civil.in' => 'El Estado Civil debe ser uno de los valores permitidos.',
+            'estado_civil.required' => 'El Estado Civil es obligatorio.',
+            'estado_civil.in' => 'El Estado Civil debe ser uno de los valores permitidos.',
             'fecha_fallecimiento.date' => 'La Fecha de Fallecimiento debe ser una fecha válida.',
         ]);
 
@@ -113,7 +113,7 @@ class CiudadanoController extends Controller
             'segundo_apellido' => 'max:255',
             'sexo' => 'required|in:M,F',
             'fecha_nacimiento' => 'required|date',
-            'id_estado_civil' => 'required|in:1,2,3,4,5,6',
+            'estado_civil' => 'required|in:1,2,3,4,5,6',
             'fecha_fallecimiento' => 'date|nullable',
         ], [
             'primer_nombre.required' => 'El Primer Nombre es obligatorio.',
@@ -126,8 +126,8 @@ class CiudadanoController extends Controller
             'sexo.in' => 'El Sexo debe ser Masculino o Femenino.',
             'fecha_nacimiento.required' => 'La Fecha de Nacimiento es obligatoria.',
             'fecha_nacimiento.date' => 'La Fecha de Nacimiento debe ser una fecha válida.',
-            'id_estado_civil.required' => 'El Estado Civil es obligatorio.',
-            'id_estado_civil.in' => 'El Estado Civil debe ser uno de los valores permitidos.',
+            'estado_civil.required' => 'El Estado Civil es obligatorio.',
+            'estado_civil.in' => 'El Estado Civil debe ser uno de los valores permitidos.',
             'fecha_fallecimiento.date' => 'La Fecha de Fallecimiento debe ser una fecha válida.',
         ]);
 
@@ -137,7 +137,7 @@ class CiudadanoController extends Controller
         $ciudadano->segundo_apellido = StringHelpers::strtoupper_createCiudadano($request->segundo_apellido);
         $ciudadano->sexo = $request->sexo;
         $ciudadano->fecha_nacimiento = $request->fecha_nacimiento;
-        $ciudadano->id_estado_civil = $request->id_estado_civil;
+        $ciudadano->estado_civil = $request->estado_civil;
         $ciudadano->fecha_fallecimiento = $request->fecha_fallecimiento ?: null;
 
         $ciudadano->save();
