fc1634084c2d8b4085cef5e111e86312ee6d1d41
slimesito <williamonfroy@gmail.com>
2025-02-18 14:19:04 -0400

    actualización de seguridad en el código

--- app/Models/Ciudadano.php
+++ app/Models/Ciudadano.php
@@ -11,31 +11,19 @@ class Ciudadano extends Model
 
     protected $connection = 'oracle';
     protected $table = 'CIUDADANO';
-    protected $primaryKey = 'id_ciudadano';
-    protected $keyType = 'string';
-    public $incrementing = false;
-    public $timestamps = false;
 
     protected $fillable = [
-        'id_ciudadano',
+        'id',
         'primer_nombre',
         'segundo_nombre',
         'primer_apellido',
         'segundo_apellido',
         'sexo',
         'rif',
-        'condicion_zurdo',
-        'id_ocupacion',
         'fecha_nacimiento',
         'id_estado_civil',
-        'id_catastro',
-        'telefono_hab',
         'telefono_movil',
         'email_principal',
-        'email_alterno',
         'fecha_fallecimiento',
-        'estatus_afiliado',
-        'cedula_extranjero',
-        'id_nacionalidad'
     ];
 }
